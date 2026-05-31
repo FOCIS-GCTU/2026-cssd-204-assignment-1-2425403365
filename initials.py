f1fe9ad23766128c7590e25dcccdacfcfe4cc49c
2425403365 <2425403365@live.gctu.edu.gh>
2026-05-31 03:28:23 +0000

    finished assigment 1

--- initials.py
+++ initials.py
@@ -1,6 +1,35 @@
+# File: initials.py
+# Description: Print out my initials in large letters
+# Assignment Number: 1
+#
+# Name: Fortune Cephas AsamaniJunior
+# STUDENT ID: 2425403365
+# Email: 2425403365@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, FCAJ, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
-def main():
-  pass #code goes here
+def main():  # Print initials: F C A as 12x10 block letters with periods
+  print()
+  print("...FCA")
+  print()
+
+  print("...FFFFFFFFFFFF........CCCCCCCCCCCC........AAAAAAAAAAAA...")
+  print("...FFFFFFFFFFFF........CCCCCCCCCCCC........AAAAAAAAAAAA...")
+  print("...FF..................CCC.................AA........AA...")
+  print("...FFFFFFFFFFFF........CCC.................AAAAAAAAAAAA...")
+  print("...FFFFFFFFFFFF........CCC.................AAAAAAAAAAAA...")
+  print("...FF..................CCC.................AA........AA...")
+  print("...FF..................CCC.................AA........AA...")
+  print("...FF..................CCC.................AA........AA...")
+  print("...FF.............**...CCCCCCCCCCCC...**...AA........AA...")
+  print("...FF.............**...CCCCCCCCCCCC...**...AA........AA...")
+
+  print()
+
+
+
 
 
 
